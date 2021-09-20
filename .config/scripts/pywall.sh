4dafe85a187a450a2bfa94f392da5dd93a411d41
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2021-09-20 16:20:38 -0400

    Cleanup & change pywal backend

--- .config/scripts/pywall.sh
+++ .config/scripts/pywall.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-${HOME}/.local/bin/wal -i ${HOME}/code/wp/ --recursive --backend haishoku --saturate 0.95 &&
-razer-cli -a
+${HOME}/.local/bin/wal -i ${HOME}/code/wp/ --recursive --backend colorz --saturate 0.95 
